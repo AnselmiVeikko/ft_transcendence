55b694de54f49eea00dd615607c0c23e07389503
shahnajsc <sschowdhury29@gmail.com>
2025-11-12 17:13:15 +0200

    Created first structure of the backend. Stack: Node.js + FastiFy + TypeScript.

---
+++ backend/src/app.ts
@@ -0,0 +1,25 @@
+import Fastify from "fastify";
+import dotenv from "dotenv"
+
+dotenv.config();
+
+const app = Fastify({ logger: true });
+const port = process.env.BACKEND_PORT? Number(process.env.BACKEND_PORT) : 3000
+
+app.get("/health", async() => ({Hello: "Backend is Running"}));
+
+const start = async () => {
+
+	try {
+		await app.listen({port});
+		app.log.info('Server is listening at port: ${port}');
+	}
+	catch (err) {
+		app.log.error(err);
+		process.exit(1);
+	}
+}
+
+start();
+
+export default app;
