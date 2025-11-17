9c38c14e36d6e9eceaf22968a5e93948ab17510f
tranhieutrung <tranhieutrung@outlook.com>
2025-11-17 12:21:44 +0200

    Fix game loop to avoid increasing ball's speed.

--- game/src/game.ts
+++ game/src/game.ts
@@ -58,45 +58,38 @@ export function initGame() {
   const playBtn = document.getElementById("2PlayerBtn")!;
 
   playWAiBtn.addEventListener("click", () => {
-    if (isRunningAI) return;
     if (isRunning2P) isRunning2P = false;
     isRunningAI = true;
     resetGame();
-    loop();
   });
 
   playBtn.addEventListener("click", () => {
-    if (isRunning2P) return;
     if (isRunningAI) isRunningAI = false;
     isRunning2P = true;
     resetGame();
-    loop();
   });
 
-// function stopGame() {
-//   // isRunning = false;
-//   // startBtn.disabled = false;
-//   ctx.fillStyle = "#fff";
-//   ctx.font = "24px Arial";
-//   ctx.fillText("Game Over", canvas.width / 2 - 60, canvas.height / 2);
-// }
   function resetGame() {
     ball.reset(canvas.width, canvas.height);
     leftPlayer.y = canvas.height / 2 - leftPlayer.height / 2;
     rightPlayer.y = canvas.height / 2 - rightPlayer.height / 2;
   }
+
+  loop()
+
   function loop() {
-    if (!isRunning2P && !isRunningAI) return;
+    if (isRunning2P || isRunningAI) {
+      ball.move();
+      ball.bounce(canvas.height);
+      checkCollision(ball, leftPlayer);
+      checkCollision(ball, rightPlayer);
+      moveLeftPaddle()
+      if (isRunning2P) moveRightPaddle();
+      else aiMove(rightPlayer, ball, canvas.height);
 
-    ball.move();
-    ball.bounce(canvas.height);
-    checkCollision(ball, leftPlayer);
-    checkCollision(ball, rightPlayer);
-    moveLeftPaddle()
-    if (isRunning2P) moveRightPaddle();
-    else aiMove(rightPlayer, ball, canvas.height);
+      if (ball.x < 0 || ball.x > canvas.width) ball.reset(canvas.width, canvas.height);
 
-    if (ball.x < 0 || ball.x > canvas.width) ball.reset(canvas.width, canvas.height);
+    }
 
     render(ctx, ball, leftPlayer, rightPlayer);
     requestAnimationFrame(loop);
