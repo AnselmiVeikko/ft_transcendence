23914ba6ebd444227155d6bfc00bd41545002bf7
shahnajsc <sschowdhury29@gmail.com>
2025-11-18 16:48:10 +0200

    token creation part commented

--- backend/src/routes/login.ts
+++ backend/src/routes/login.ts
@@ -1,7 +1,10 @@
 import { FastifyInstance } from "fastify";
 import { prisma } from "../plugins/prisma";
-import { bcrypt } from "bcrypt";
-import { jwt } from "jsonwebtoken";
+import dotenv from "dotenv";
+import bcrypt from "bcrypt";
+//import jwt from "jsonwebtoken";
+
+dotenv.config();
 
 export default async function loginRoutes(app: FastifyInstance) {
     app.post("/user/login", async (request, reply) => {
@@ -9,12 +12,13 @@ export default async function loginRoutes(app: FastifyInstance) {
             username?: string;
             password?: string;
         };
-    
+
     if (!username || !password) {
         return reply.status(400).send({ error: "All fields are required."});
     }
 
     const existingUser = await prisma.user_info.findUnique({ where: { username }});
+
     if (!existingUser) {
         return reply.status(401).send({ error: "User does not exist"});
     }
@@ -26,15 +30,16 @@ export default async function loginRoutes(app: FastifyInstance) {
         return reply.status(401).send({ error: "Invalid credentials."});
     }
 
-    const token = jwt.sign(
-        { userId: existingUser.id, username: existingUser.username },
-        ProcessingInstruction.env.JWT_SECRET || "dev-secret",
-        { expiresIn: "7d" }
-    )
+    // const token = jwt.sign(
+    //     { userId: existingUser.id, username: existingUser.username },
+    //     ProcessingInstruction.env.JWT_SECRET || "dev-secret",
+    //     { expiresIn: "7d" }
+    // )
 
     return reply.status(200).send({
         user: "Login succesful",
-        token,
-        user: { id: existingUser.id, username: existingUser.username }
+        // token,
+        // user: { id: existingUser.id, username: existingUser.username }
+    });
     });
 }
