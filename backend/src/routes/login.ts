efb646c5e79f071aaf0396a3ebeef2bd05e0286e
Shahnaj Sharmin Chowdhury <168670941+shahnajsc@users.noreply.github.com>
2025-11-19 11:51:37 +0200

    Backend main (#11)
    * Created first structure of the backend. Stack: Node.js + FastiFy + TypeScript.
    
    * User registration endpoint function added
    
    * Prisma client created for DB connection
    
    * Updated registration routes
    
    * user_info model updated. added creation and update date
    
    * removed driverAdapter
    
    * updated dotenv for database URL
    
    * Dependency added for prisma
    
    * Dependency added for prisma
    
    * instruction for database added
    
    * Made a very simle module to accept login in the backend.
    
    * Added encrypting for passwords
    
    * added docker service for backend
    
    * Added Dockerfile for backend
    
    * Updated listenging  port to align with fastify
    
    * Added JWT generator to login function.
    
    ---------
    
    Co-authored-by: Anselmi Henttonen <AnselmiVeikko@gmail.com>
    Co-authored-by: AnselmiVeikko <167296462+AnselmiVeikko@users.noreply.github.com>

---
+++ backend/src/routes/login.ts
@@ -0,0 +1,40 @@
+import { FastifyInstance } from "fastify";
+import { prisma } from "../plugins/prisma";
+import { bcrypt } from "bcrypt";
+import { jwt } from "jsonwebtoken";
+
+export default async function loginRoutes(app: FastifyInstance) {
+    app.post("/user/login", async (request, reply) => {
+        const { username, password } = request.body as {
+            username?: string;
+            password?: string;
+        };
+    
+    if (!username || !password) {
+        return reply.status(400).send({ error: "All fields are required."});
+    }
+
+    const existingUser = await prisma.user_info.findUnique({ where: { username }});
+    if (!existingUser) {
+        return reply.status(401).send({ error: "User does not exist"});
+    }
+
+    //Check the crypted password
+    const passCheck = await bcrypt.compare(password, existingUser.password);
+
+    if (!passCheck) {
+        return reply.status(401).send({ error: "Invalid credentials."});
+    }
+
+    const token = jwt.sign(
+        { userId: existingUser.id, username: existingUser.username },
+        ProcessingInstruction.env.JWT_SECRET || "dev-secret",
+        { expiresIn: "7d" }
+    )
+
+    return reply.status(200).send({
+        user: "Login succesful",
+        token,
+        user: { id: existingUser.id, username: existingUser.username }
+    });
+}
