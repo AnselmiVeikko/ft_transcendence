98f753d65d64372e5c4cc6cf8bd48ac8884c4bfb
shahnajsc <sschowdhury29@gmail.com>
2025-11-20 12:43:51 +0200

    Response msg and endpoint updated

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -10,31 +10,31 @@ const app = Fastify({ logger: true });
 const port = process.env.BACKEND_PORT? Number(process.env.BACKEND_PORT) : 3000
 
 async function buildServer() {
-  // Register CORS plugin
-  await app.register(cors, {
-    origin: "*"
-  });
-  app.get("/health", async () => ({ Hello: "Backend is running...." }));
+	// Later make it more secure ***
+	await app.register(cors, {
+		origin: "*"
+	});
 
-  app.register(registrationRoutes);
-  app.register(loginRoutes);
+	// Health check
+	app.get("/api//health", async () => ({ Hello: "Backend is running...." }));
+
+	// Endpoints
+	app.register(registrationRoutes);
+	app.register(loginRoutes);
 }
 
-
 const start = async () => {
-
-  try {
-    await buildServer();
-    await app.listen({port, host: '0.0.0.0'});
-    app.log.info('Server is listening at port: ${port}');
-   }
-  catch (err) {
-    app.log.error(err);
-    process.exit(1);
-  }
+	try {
+		await buildServer();
+		await app.listen({port, host: '0.0.0.0'});
+		app.log.info('Server is listening at port: ${port}');
+	}
+	catch (err) {
+		app.log.error(err);
+		process.exit(1);
+	}
 }
 
 start();
 
 export default app;
-
