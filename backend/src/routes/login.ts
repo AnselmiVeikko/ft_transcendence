4f2d7fee9cc79d5966b0c3033345402526e56118
Anselmi Henttonen <AnselmiVeikko@gmail.com>
2025-11-21 09:35:48 +0200

    Login.ts now sends set-cookie header in the reply.

--- backend/src/routes/login.ts
+++ backend/src/routes/login.ts
@@ -5,10 +5,7 @@ import { jwt } from "jsonwebtoken";
 
 export default async function loginRoutes(app: FastifyInstance) {
     app.post("/user/login", async (request, reply) => {
-        const { username, password } = request.body as {
-            username?: string;
-            password?: string;
-        };
+        const { username, password } = request.body as { username?: string; password?: string; };
     
     if (!username || !password) {
         return reply.status(400).send({ error: "All fields are required."});
@@ -16,7 +13,7 @@ export default async function loginRoutes(app: FastifyInstance) {
 
     const existingUser = await prisma.user_info.findUnique({ where: { username }});
     if (!existingUser) {
-        return reply.status(401).send({ error: "User does not exist"});
+        return reply.status(401).send({ error: "Invalid credentials."});
     }
 
     //Check the crypted password
@@ -28,13 +25,18 @@ export default async function loginRoutes(app: FastifyInstance) {
 
     const token = jwt.sign(
         { userId: existingUser.id, username: existingUser.username },
-        ProcessingInstruction.env.JWT_SECRET || "dev-secret",
+        process.env.JWT_SECRET || "dev-secret",
         { expiresIn: "7d" }
     )
 
-    return reply.status(200).send({
-        user: "Login succesful",
-        token,
-        user: { id: existingUser.id, username: existingUser.username }
+    reply.setCookie("auth_token", token, {
+        httpOnly: true,
+        secure: process.env.NODE_ENV === "production",
+        sameSite: "lax",
+        path: "/",
+        maxAge: 7 * 24 * 60 * 60
+        })
+        .status(200)
+        .send({ message:"Login succesful", user: { id: existingUser.id, username: existingUser.username } });
     });
 }
