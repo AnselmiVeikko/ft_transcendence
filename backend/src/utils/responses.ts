615b7f4a41e9703c34fb3bb5858160e27bb4e4da
Anselmi Henttonen <AnselmiVeikko@gmail.com>
2025-11-25 16:36:26 +0200

    Removed JWT from loginSuccess message, all tokens are sent with cookies automatically.

--- backend/src/utils/responses.ts
+++ backend/src/utils/responses.ts
@@ -1,7 +1,6 @@
-export function loginSuccess(user: { id: number, username: string }, token: string) {
+export function loginSuccess(user: { id: number, username: string }) {
     return {
         message: "Login succesful",
-        token,
         user: { id: user.id, username: user.username },
     };
 }
