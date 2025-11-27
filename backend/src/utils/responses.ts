3784d5434e514f668bc61a12648c96096aeb607e
shahnajsc <sschowdhury29@gmail.com>
2025-11-27 11:58:43 +0200

    Dataset name changed to data from user

--- backend/src/utils/responses.ts
+++ backend/src/utils/responses.ts
@@ -1,24 +1,30 @@
 export function loginSuccess(user: { userId: number, userName: string }) {
-    return {
-        message: "Login succesful",
-        user: { userId: user.userId, userName: user.userName },
-    };
+	return {
+		message: "Login succesful",
+		user: {
+			userId: user.userId,
+			userName: user.userName
+		},
+	};
 }
 
 export function RegistrationSuccess(user: { userId: number, userName: string }) {
-    return {
-        message: "Registration succesful",
-        user: { userId: user.userId, userName: user.userName },
-    };
+	return {
+		message: "Registration succesful",
+		data: {
+			userId: user.userId,
+			userName: user.userName
+		},
+	};
 }
 
 export function ProfilePersonal(userProfile: {userId: number, userName: string, email: string}) {
-    return {
-        message: "Profile retrieved successfully",
-        userProfile: {
-            userId: userProfile.userId,
-            userName: userProfile.userName,
-            email: userProfile.email
-        },
-    };
+	return {
+		message: "Profile retrieved successfully",
+		data: {
+			userId: userProfile.userId,
+			userName: userProfile.userName,
+			email: userProfile.email
+		},
+	};
 }
