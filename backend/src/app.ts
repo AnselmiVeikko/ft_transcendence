d20f3dc995eea479ef31c33cbfad04798fdedc99
shahnajsc <sschowdhury29@gmail.com>
2025-11-27 11:58:10 +0200

    Formatting

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -14,32 +14,30 @@ const port = process.env.BACKEND_PORT? Number(process.env.BACKEND_PORT) : 3000
 
 
 async function buildServer() {
-  // Register CORS plugin
-  await app.register(cors, {
-    origin: "*"
-  });
-  app.get("/health", async () => ({ Hello: "Backend is running...." }));
-  app.register(fastifyCookie);
-  app.register(registrationRoutes);
-  app.register(loginRoutes);
-  app.register(profileRoutes);
+	// Register CORS plugin
+	await app.register(cors, {
+		origin: "*"
+	});
+	app.get("/health", async () => ({ Hello: "Backend is running...." }));
+	app.register(fastifyCookie);
+	app.register(registrationRoutes);
+	app.register(loginRoutes);
+	app.register(profileRoutes);
 }
 
 
 const start = async () => {
-
-  try {
-    await buildServer();
-    await app.listen({port, host: '0.0.0.0'});
-    app.log.info('Server is listening at port: ${port}');
-   }
-  catch (err) {
-    app.log.error(err);
-    process.exit(1);
-  }
+	try {
+		await buildServer();
+		await app.listen({port, host: '0.0.0.0'});
+		app.log.info('Server is listening at port: ${port}');
+	}
+	catch (err) {
+		app.log.error(err);
+		process.exit(1);
+	}
 }
 
 start();
 
 export default app;
-
