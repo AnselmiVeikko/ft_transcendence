76ded2b470f8b7efe29c90f79bc798d4e1937622
eelaine <eelaine@student.hive.fi>
2025-11-28 12:08:31 +0200

    implemented light and dark mode backgrounds

--- frontend/src/components/Menu.tsx
+++ frontend/src/components/Menu.tsx
@@ -4,12 +4,6 @@ import { useTranslation } from 'react-i18next'
 import SettingsMenu from './SettingsMenu'
 import { FaPlay, FaTrophy, FaChartBar } from 'react-icons/fa';
 
-document.documentElement.classList.toggle(
-  "dark",
-  localStorage.theme === "dark" ||
-    (!("theme" in localStorage) && window.matchMedia("(prefers-color-scheme: dark)").matches),
-);
-
 interface MenuCardProps {
   title: string;
   colorClass: string;
@@ -21,6 +15,8 @@ const colorClasses = {
 	start: 'bg-gradient-to-b from-indigo-700 to-blue-300 hover:bg-gradient-to-r hover:to-indigo-200',
 	tournament: 'bg-gradient-to-b from-violet-700 to-blue-300 hover:bg-gradient-to-r hover:to-violet-200',
 	stats: 'bg-gradient-to-b from-fuchsia-600 to-blue-300 hover:bg-gradient-to-r hover:to-fuchsia-200',
+	bgLight: 'bg-linear-to-b from-white to-slate-100',
+	bgDark: 'bg-linear-to-r dark:from-fuchsia-800 dark:to-cyan-800',
 };
 
 const MenuCard = ({ title, colorClass, icon, onClick }: MenuCardProps) => (
@@ -28,8 +24,7 @@ const MenuCard = ({ title, colorClass, icon, onClick }: MenuCardProps) => (
         onClick={onClick}
         className={`w-full lg:w-60 lg:h-90 flex flex-col items-center justify-center p-6 
                     text-white rounded-xl shadow-lg transition duration-300 
-                    transform hover:scale-[1.07] hover:shadow-xl ${colorClass}`}
-    >
+                    transform hover:scale-[1.07] hover:shadow-xl ${colorClass}`}>
         <div className="text-4xl mb-3">{icon}</div>
         <span className="text-xl font-bold uppercase tracking-wider">{title}</span>
     </button>
@@ -37,6 +32,7 @@ const MenuCard = ({ title, colorClass, icon, onClick }: MenuCardProps) => (
 // ---------------------------------------
 
 const Menu = () => {
+
   const handleButtonClick = (buttonName: string) => {
     console.log(`${buttonName} clicked`);
   };
@@ -53,6 +49,7 @@ const Menu = () => {
 
   return (
     <div className="relative h-screen overflow-hidden">
+		
       <SettingsMenu />
       <Header
         appName="PONG"
@@ -61,10 +58,10 @@ const Menu = () => {
         onLogout={handleLogout}
       />
       <div 
-        className="bg-linear-to-b from-white to-slate-100 dark:bg-linear-to-b dark:from-slate-900 dark:to-blue-900 flex items-center justify-center p-4"
+        className={`${colorClasses.bgLight} ${colorClasses.bgDark} flex items-center justify-center p-4`}
         style={{ height: 'calc(100vh - 3.5rem)' }} // make sure content fits below header
       >
-        <div className="flex flex-col lg:flex-row items-center gap-12 max-w-7xl mx-auto">
+        <div className="flex flex-col lg:flex-row items-center gap-12 max-w-7xl mx-auto z-10">
           
           <MenuCard
             title={t('single_match')}
