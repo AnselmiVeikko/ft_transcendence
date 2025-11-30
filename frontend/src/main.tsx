40a6ec5c60f4cb555621e3ccd313990efccfbdc4
Finu <finnantiku@gmail.com>
2025-12-01 00:19:20 +0200

    creates friends page

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,38 +1,40 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import './index.css'
-import App from './App.tsx'
-import './i18n.ts';
-import LogIn from './components/LogIn.tsx'
-import Menu from './components/Menu.tsx'
-import Register from './components/Register.tsx'
-import ProtectedRoutes from './utils/ProtectedRoutes.tsx';
-import PageNotFound from './components/PageNotFound.tsx';
+import "./index.css";
+import App from "./App.tsx";
+import "./i18n.ts";
+import LogIn from "./components/LogIn.tsx";
+import Menu from "./components/Menu.tsx";
+import Register from "./components/Register.tsx";
+import ProtectedRoutes from "./utils/ProtectedRoutes.tsx";
+import PageNotFound from "./components/PageNotFound.tsx";
+import FriendsList from "./components/FriendsList.tsx";
 
 const router = createBrowserRouter([
-	{
-		path: "/",
-		element: <App />,
-		children: [
-			{ path: "/", element: <LogIn /> },
-			{ path: "/register", element: <Register /> },
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      { path: "/", element: <LogIn /> },
+      { path: "/register", element: <Register /> },
 
-			// Protected routes (for logged-in users only)
-			{
-				element: <ProtectedRoutes />,
-				children: [
-					{ path: "/menu", element: <Menu /> }
-				],
-			},
+      // Protected routes (for logged-in users only)
+      {
+        element: <ProtectedRoutes />,
+        children: [
+          { path: "/menu", element: <Menu /> },
+          { path: "/friends", element: <FriendsList /> },
+        ],
+      },
 
-			{ path: "*", element: <PageNotFound /> }
-		],
-	},
+      { path: "*", element: <PageNotFound /> },
+    ],
+  },
 ]);
 
-createRoot(document.getElementById('root')!).render(
-	<StrictMode>
-			<RouterProvider router={router} />
-	</StrictMode>,
+createRoot(document.getElementById("root")!).render(
+  <StrictMode>
+    <RouterProvider router={router} />
+  </StrictMode>
 );
