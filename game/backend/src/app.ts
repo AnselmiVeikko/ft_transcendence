cd941a0907157ffa666d7c6412aacc63514dec96
tranhieutrung <tranhieutrung@outlook.com>
2025-12-09 12:26:38 +0200

    Update game/backend's tsc config.

--- game/backend/src/app.ts
+++ game/backend/src/app.ts
@@ -1,7 +1,7 @@
-import express, { Request, Response } from 'express';
+import express, { type Request, type Response } from 'express';
 
 const app = express();
-const port = process.env.PORT || 4000;
+const port = Number(process.env.PORT) || 4000;
 
 app.use(express.json());
 
@@ -9,8 +9,9 @@ app.get('/', (req: Request, res: Response) => {
   res.send('Hello World!');
 });
 
-app.listen(port, () => {
-  console.log(`Server running on http://localhost:${port}`);
+app.listen(port, '0.0.0.0', () => {
+  console.log(`Server running on http://0.0.0.0:${port}`);
 });
+
 
 export default app;
