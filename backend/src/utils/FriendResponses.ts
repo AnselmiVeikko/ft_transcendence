957983cfbe1f6c2f3cfc44628b807d2ec102815e
shahnajsc <sschowdhury29@gmail.com>
2025-12-11 17:34:35 +0200

    Fastify schema and logic changed

--- backend/src/utils/FriendResponses.ts
+++ backend/src/utils/FriendResponses.ts
@@ -1,27 +1,30 @@
-export function FRSendSuccess(request: { senderId: string, receiverId: string}) {
+export function FRSendSuccess(request: { friendRId: string, senderId: string, receiverId: string}) {
 	return {
 		message: "Request sent",
 		data: {
+			friendRId: request.friendRId,
 			senderId: request.senderId,
 			receiverId: request.receiverId,
 		},
 	};
 }
 
-export function FRAcceptSuccess(request: { senderId: string, receiverId: string}) {
+export function FRAcceptSuccess(request: { friendRId: string, senderId: string, receiverId: string}) {
 	return {
 		message: "Request Accepted",
 		data: {
+			friendRId: request.friendRId,
 			senderId: request.senderId,
 			receiverId: request.receiverId,
 		},
 	};
 }
 
-export function FRDecclineSuccess(request: { senderId: string, receiverId: string}) {
+export function FRDecclineSuccess(request: { friendRId: string, senderId: string, receiverId: string}) {
 	return {
 		message: "Request Declined",
 		data: {
+			friendRId: request.friendRId,
 			senderId: request.senderId,
 			receiverId: request.receiverId,
 		},
