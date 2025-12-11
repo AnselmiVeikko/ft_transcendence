a8bf7479c1439bb8f57d79ec73eea3a57ece0f64
shahnajsc <sschowdhury29@gmail.com>
2025-12-11 17:34:22 +0200

    Fastify schema and logic changed

--- backend/src/schemas/FriendSchema.ts
+++ backend/src/schemas/FriendSchema.ts
@@ -1,39 +1,41 @@
 import { Type } from "@sinclair/typebox";
 
 export const FRSendBodySchema = Type.Object({
-	senderId: Type.String(),
 	receiverId: Type.String(),
 })
 
 export const FRSendResponseSchema = Type.Object({
 	message: Type.String(),
 	data: Type.Object({
+		friendRId: Type.String(),
 		senderId: Type.String(),
 		receiverId: Type.String(),
 	}),
 })
 
 export const FRAcceptBodySchema = Type.Object({
+	friendRId: Type.String(),
 	senderId: Type.String(),
-	receiverId: Type.String(),
 })
 
 export const FRAcceptResponseSchema = Type.Object({
 	message: Type.String(),
 	data: Type.Object({
+		friendRId: Type.String(),
 		senderId: Type.String(),
 		receiverId: Type.String(),
 	})
 })
 
 export const FRDeclineBodySchema = Type.Object({
+	friendRId: Type.String(),
 	senderId: Type.String(),
-	receiverId: Type.String(),
 })
 
 export const FRDeclineResponseSchema = Type.Object({
 	message: Type.String(),
 	data: Type.Object({
+		friendRId: Type.String(),
 		senderId: Type.String(),
 		receiverId: Type.String(),
 	})
