46cef6b7a0511a6780896650fb1a8c7537fd4491
Anselmi Henttonen <AnselmiVeikko@gmail.com>
2025-12-11 16:57:26 +0200

    User's online status now changes upon login and logoout.

--- backend/src/routes/Logout.ts
+++ backend/src/routes/Logout.ts
@@ -24,13 +24,16 @@ export default async function logoutRoutes(app: FastifyInstance) {
            return reply.status(401).send(errorResponse(401, "User does not exist"));
         }
 
+        
         clearCookies(reply);
-
-        //TODO: toggle person offline in the database
+        
         const responseUser = {
             userId: user.userId,
             userName: user.userName,
         };
+
+        await prisma.user_info.update({ where: { userId: user.userId }, data: { status: 'OFFLINE' } }); //TODO: Wrap in try/catch
+
         return reply.status(200).send(logoutSuccess(responseUser));
     });
 }
