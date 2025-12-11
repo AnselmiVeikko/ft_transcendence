edc98a04f0b029987ac9ce2698f882fd6fc4ee79
shahnajsc <sschowdhury29@gmail.com>
2025-12-11 17:34:28 +0200

    Fastify schema and logic changed

--- backend/src/schemas/UserSchema.ts
+++ backend/src/schemas/UserSchema.ts
@@ -74,7 +74,7 @@ export const ProfileAllResponseSchema = Type.Object({
 
 // Error Response
 export const ErrorResponseSchema = Type.Object({
-   // statusCode: Type.Number(),
-    // error: Type.String(),
+    statusCode: Type.Number(),
+    error: Type.String(),
     message: Type.String(),
 });
