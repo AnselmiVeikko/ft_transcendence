df3a82a3d5e9a26ffaad0abab20389b7eee6d754
shahnajsc <sschowdhury29@gmail.com>
2025-12-15 21:48:24 +0200

    Registered friend list endpoints

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -7,6 +7,7 @@ import registrationRoutes from "./routes/Register";
 import loginRoutes from "./routes/Login";
 import profileRoutes from "./routes/Profile";
 import FriendRequest from "./routes/FriendRequest";
+import FriendList from "./routes/FriendList";
 
 dotenv.config();
 
@@ -20,13 +21,14 @@ async function buildServer() {
 		origin: "http://localhost:5173",
 		credentials: true
 	});
-	
+
 	app.get("/health", async () => ({ Hello: "Backend is running...." }));
-	
+
 	app.register(fastifyCookie);
 	app.register(registrationRoutes);
 	app.register(loginRoutes);
 	app.register(profileRoutes);
+	app.register(FriendList);
 	app.register(FriendRequest);
 }
 
