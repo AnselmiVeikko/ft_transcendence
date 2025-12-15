19586b939f68d60a5a9c96291bb0cfb1843fa05c
shahnajsc <sschowdhury29@gmail.com>
2025-12-15 21:51:13 +0200

    Commented status code

--- backend/src/schemas/UserSchema.ts
+++ backend/src/schemas/UserSchema.ts
@@ -74,7 +74,7 @@ export const ProfileAllResponseSchema = Type.Object({
 
 // Error Response
 export const ErrorResponseSchema = Type.Object({
-    statusCode: Type.Number(),
+    //statusCode: Type.Number(),
     error: Type.String(),
     message: Type.String(),
 });
