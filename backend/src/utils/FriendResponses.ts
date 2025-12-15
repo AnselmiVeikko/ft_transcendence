a41359f92a0d7863d13103e1c168a0916180bb4c
shahnajsc <sschowdhury29@gmail.com>
2025-12-15 21:51:59 +0200

    Created response schemas for friend list and delete friend request endpoints

--- backend/src/utils/FriendResponses.ts
+++ backend/src/utils/FriendResponses.ts
@@ -1,3 +1,38 @@
+// Friend List Responses
+export function CurrentList(list: any[]) {
+	return {
+		message: "Friends List",
+		data: list.map(u => ({
+			friendRId: u.friendRId,
+			userId: u.userId,
+			userName: u.userName
+		})),
+	};
+}
+
+export function PendingList(list: any[]) {
+	return {
+		message: "Friends Pending List",
+		data: list.map(u => ({
+			friendId: u.friendId,
+			userId: u.userId,
+			userName: u.userName
+		})),
+	};
+}
+
+export function SuggestionList(list: any[]) {
+	return {
+		message: "Friends Suggestion List",
+		data: list.map(u => ({
+			friendId: u.friendId,
+			userId: u.userId,
+			userName: u.userName
+		})),
+	};
+}
+
+// Friend Request Responses
 export function FRSendSuccess(request: { friendRId: string, senderId: string, receiverId: string}) {
 	return {
 		message: "Request sent",
@@ -27,6 +62,15 @@ export function FRDecclineSuccess(request: { friendRId: string, senderId: string
 			friendRId: request.friendRId,
 			senderId: request.senderId,
 			receiverId: request.receiverId,
+		},
+	};
+}
+
+export function FRDeleteSuccess(request: { friendRId: string}) {
+	return {
+		message: "Request Deleted",
+		data: {
+			friendRId: request.friendRId,
 		},
 	};
 }
