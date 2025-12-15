fb0368e2e0535cafc90d8b2353fe3e59b08a082a
shahnajsc <sschowdhury29@gmail.com>
2025-12-15 21:50:49 +0200

    Created schemas for friend list and delete friend request endpoints

--- backend/src/schemas/FriendSchema.ts
+++ backend/src/schemas/FriendSchema.ts
@@ -1,4 +1,55 @@
 import { Type } from "@sinclair/typebox";
+import { syncBuiltinESMExports } from "module";
+
+// Friend List Schemas
+
+export const FLCurrentQuerySchema = Type.Object({
+	// userId will retrieve from cookies
+})
+
+export const FLCurrentResponseSchema = Type.Object({
+	message: Type.String(),
+	data: Type.Array(
+		Type.Object({
+			friendRId: Type.String(),
+			userId: Type.String(),
+			userName: Type.String(),
+			//avatar:
+		}),
+	),
+})
+
+export const FLPendingQuerySchema = Type.Object({
+	// userId will retrieve from cookies
+})
+
+export const FLPendingResponseSchema = Type.Object({
+	message: Type.String(),
+	data: Type.Array(
+		Type.Object({
+			userId: Type.String(),
+			userName: Type.String(),
+			//avatar:
+		}),
+	),
+})
+
+export const FLSuggestionQuerySchema = Type.Object({
+	// userId will retrieve from cookies
+})
+
+export const FLSuggestionResponseSchema = Type.Object({
+	message: Type.String(),
+	data: Type.Array(
+		Type.Object({
+			userId: Type.String(),
+			userName: Type.String(),
+			//avatar:
+		}),
+	),
+})
+
+// Friend Request Schemas
 
 export const FRSendBodySchema = Type.Object({
 	receiverId: Type.String(),
@@ -7,7 +58,6 @@ export const FRSendBodySchema = Type.Object({
 export const FRSendResponseSchema = Type.Object({
 	message: Type.String(),
 	data: Type.Object({
-		friendRId: Type.String(),
 		senderId: Type.String(),
 		receiverId: Type.String(),
 	}),
@@ -38,5 +88,16 @@ export const FRDeclineResponseSchema = Type.Object({
 		friendRId: Type.String(),
 		senderId: Type.String(),
 		receiverId: Type.String(),
+	})
+})
+
+export const FRDeleteQuerySchema = Type.Object({
+	friendRId: Type.String(),
+})
+
+export const FRDeleteResponseSchema = Type.Object({
+	message: Type.String(),
+	data: Type.Object({
+		friendRId: Type.String(),
 	})
 })
