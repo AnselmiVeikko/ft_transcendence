1897b9b054f447ba2b896722361d058991be413b
Anselmi Henttonen <AnselmiVeikko@gmail.com>
2025-12-19 18:27:35 +0200

    Created basic functionality for matchmaking

--- backend/src/routes/Matchmaking.ts
+++ backend/src/routes/Matchmaking.ts
@@ -1,6 +1,9 @@
 import { FastifyInstance, FastifyRequest, FastifyReply } from "fastify";
 import { ErrorResponseSchema } from "../schemas/UserSchema";
 import { verifyAccess } from "../utils/auth";
+import { errorResponse } from "../utils/UserResponses";
+import { MatchmakingResponseSchema } from "../schemas/GameSchema";
+import prisma from "../plugins/prisma";
 
 export default async function matchmakingRequest(app: FastifyInstance) {
     app.post( "/api/game/matchmaking", {
@@ -15,6 +18,30 @@ export default async function matchmakingRequest(app: FastifyInstance) {
         const userId = verifyAccess(request, reply);
         if (!userId)
             return ;
+
+        const user = await prisma.user_info.findUnique({
+            where: { userId: userId }
+        });
+
+        if (!user) {
+            return reply.status(200).send(errorResponse(400, "User profile not found"));
+        }
+
+        const updated = await prisma.game_match.updateMany({
+            where: {
+                status:  "MATCHMAKING",
+                playerTwoId: null,
+            },
+            data: {
+                status: "IN_PROGRESS",
+                playerTwoId: userId,
+            },
+        });
+
+        if (updated.count === 0){
+            await prisma.game_match.create({ playerOneId: userId});
+        }
+
     }
 )
 }
