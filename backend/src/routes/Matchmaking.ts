39a759f06109976b93f6e5a2badc8f013184e343
Anselmi Henttonen <AnselmiVeikko@gmail.com>
2026-01-09 15:49:00 +0200

    Finished a funcitonal matchmaking handle

--- backend/src/routes/Matchmaking.ts
+++ backend/src/routes/Matchmaking.ts
@@ -2,44 +2,59 @@ import { FastifyInstance, FastifyRequest, FastifyReply } from "fastify";
 import { ErrorResponseSchema } from "../schemas/UserSchema";
 import { verifyAccess } from "../utils/auth";
 import { errorResponse } from "../utils/UserResponses";
-import { MatchmakingResponseSchema } from "../schemas/GameSchema";
+import { MatchCreatedResponseSchema, MatchFoundResponseSchema } from "../schemas/GameSchema";
 import prisma from "../plugins/prisma";
+import { MatchCreated, MatchFound } from "../utils/GameResponses";
 
 export default async function matchmakingRequest(app: FastifyInstance) {
     app.post( "/api/game/matchmaking", {
         schema: {
             response: {
-                 200: MatchmakingResponseSchema,
+                 200: MatchFoundResponseSchema || MatchCreatedResponseSchema,
                 default: ErrorResponseSchema,
             },
         },
     },
     async (request: FastifyRequest, reply: FastifyReply) => {
-        const userId = await verifyAccess(request, reply);
-        if (!userId)
-            return ;
+        try {
+            const userId = await verifyAccess(request, reply);
+            if (!userId)
+                return ;
 
-        const user = await prisma.user_info.findUnique({
-            where: { userId: userId }
-        });
+            const user = await prisma.user_info.findUnique({
+                where: { userId: userId }
+            });
 
-        if (!user) {
-            return reply.status(200).send(errorResponse(400, "User profile not found"));
-        }
+            if (!user) {
+                return reply.status(401).send(errorResponse(400, "User profile not found"));
+            }
 
-        const updated = await prisma.game_match.updateMany({
-            where: {
-                status:  "MATCHMAKING",
-                playerTwoId: null,
-            },
-            data: {
-                status: "IN_PROGRESS",
-                playerTwoId: userId,
-            },
-        });
+            const availableMatch = await prisma.game_match.findFirst({
+                where: {
+                    status:  "MATCHMAKING",
+                    playerTwoId: null,
+                }
+            });
 
-        if (updated.count === 0){
-            await prisma.game_match.create({ playerOneId: userId});
+            if (availableMatch) {
+                const updatedMatch = await prisma.game_match.update({
+                    where: { matchId: availableMatch.matchId },
+                    data: {
+                        status: "STARTING",
+                        playerTwoId: userId,
+                    }
+                });
+
+                return reply.status(200).send(MatchFound(updatedMatch));
+            }
+            else {
+                const newMatch = await prisma.game_match.create({ data: { playerOneId: userId } });
+
+                return reply.status(200).send(MatchCreated(newMatch));
+            }
+        } catch (error) {
+            app.log.error(error);
+            return reply.status(500).send(errorResponse(500, "Internal server error during matchmaking"));
         }
 
     }
