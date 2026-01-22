2ca9f41c283bc03434162c7f747f3d18e4f4b02c
eelaine <eelaine@student.hive.fi>
2026-01-22 11:17:20 +0200

    new logout hook

---
+++ frontend/src/hooks/useLogout.ts
@@ -0,0 +1,27 @@
+import { useNavigate } from 'react-router-dom';
+
+export const useLogout = () => {
+  const navigate = useNavigate();
+
+  const logout = async () => {
+    try {
+      const response = await fetch('http://localhost:3000/api/user/logout', {
+        method: 'POST',
+        credentials: 'include',
+      });
+
+      if (!response.ok) {
+        if (response.status === 401) navigate("/");
+        throw new Error("Logout failed");
+      }
+
+      const result = await response.json();
+      if (result.message === "Logout succesful" || result.message === "Logout successful") {
+        navigate("/");
+      }
+    } catch (e) {
+      console.error('Failed to logout:', e);
+    }
+  };
+  return { logout };
+};
