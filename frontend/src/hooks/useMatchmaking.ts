21c51afc9c7c7542c0df8eceaaf86f6819a6ae72
eelaine <eelaine@student.hive.fi>
2026-01-22 11:17:28 +0200

    new matchmaking hook

---
+++ frontend/src/hooks/useMatchmaking.ts
@@ -0,0 +1,86 @@
+import { useState, useEffect, useRef } from 'react';
+
+export const useMatchmaking = () => {
+  const [isWaiting, setIsWaiting] = useState(false);
+  const [gameStarted, setGameStarted] = useState(false);
+  const [matchId, setMatchId] = useState('');
+  const [gameToken, setGameToken] = useState('');
+  const intervalRef = useRef<ReturnType<typeof setInterval> | null>(null);
+
+  const MATCHMAKING_API = 'http://localhost:3000/api/game/matchmaking';
+  const STATUS_API = 'http://localhost:3000/api/game/matchstatus';
+
+  const gameReady = async () => {
+    setGameStarted(true);
+    setIsWaiting(false);
+    stopPolling();
+  };
+  const startMatchmaking = async () => {
+    try {
+      const response = await fetch(MATCHMAKING_API, {
+        method: 'POST',
+        credentials: 'include',
+      });
+      if (response.status === 409) {
+		setIsWaiting(true);
+	  }
+      const result = await response.json();
+
+      console.log(result.message);
+      setGameToken(result.data.gameToken);
+      setMatchId(result.data.matchId);
+      setIsWaiting(true);
+      if (result.message === 'Match found') return gameReady();
+	  console.log("gameStarted: ", gameStarted);
+  	  console.log("isWaiting: ", isWaiting);
+      console.log("matchId: ", matchId);
+    } catch (e) {
+      console.error('Matchmaking error:', e);
+    }
+  };
+
+  const checkStatus = async () => {
+    if (!matchId) return;
+    try {
+      const response = await fetch(STATUS_API, {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ matchId }),
+        credentials: 'include',
+      });
+      const result = await response.json();
+      console.log(result.data?.matchStatus);
+      if (
+        result.data?.matchStatus === 'STARTING' ||
+        result.data?.matchStatus === 'IN_PROGRESS'
+      ) {
+        return gameReady();
+      }
+    } catch (e) {
+      console.error('Status check error:', e);
+    }
+  };
+
+  const stopPolling = () => {
+    if (intervalRef.current) {
+      clearInterval(intervalRef.current);
+      intervalRef.current = null;
+    }
+  };
+
+  //start matchmaking on load
+  useEffect(() => {
+    startMatchmaking();
+    return () => stopPolling();
+  }, []);
+
+  // polling
+  useEffect(() => {
+    if (isWaiting && matchId && !gameStarted) {
+      intervalRef.current = setInterval(checkStatus, 3000);
+    }
+    return () => stopPolling();
+  }, [isWaiting, matchId, gameStarted]);
+
+  return { isWaiting, gameStarted, matchId, gameToken };
+};
