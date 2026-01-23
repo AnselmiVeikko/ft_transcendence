696bfd7d6847c60b1191c778319704d2614919f8
Anselmi Henttonen <AnselmiVeikko@gmail.com>
2026-01-23 16:41:40 +0200

    Fixed a typo

--- backend/src/routes/Matchmaking.ts
+++ backend/src/routes/Matchmaking.ts
@@ -55,7 +55,7 @@ export default async function matchmakingRequest(app: FastifyInstance) {
                 const updatedMatch = await prisma.game_match.update({
                     where: { matchId: availableMatch.matchId },
                     data: {
-                        status: "IN PROGRESS",
+                        status: "IN_PROGRESS",
                         playerTwoId: userId,
                     }
                 });
