81f4a3437199b8aeb1db838d6f837d6014944b7b
shahnajsc <sschowdhury29@gmail.com>
2026-01-28 12:22:15 +0200

    Added profile update feature

---
+++ backend/src/schemas/ProfileSchema.ts
@@ -0,0 +1,16 @@
+import { Type } from "@sinclair/typebox";
+
+export const ProfileUpdateSchema = Type.Object({
+	userName: Type.Optional(Type.String()),
+	email: Type.Optional(Type.String()),
+	//password: Type.Optional(Type.String()),
+})
+
+export const ProfileUpdateResponseSchema = Type.Object({
+	message: Type.String(),
+	data: Type.Object({
+		userId: Type.String(),
+		userName: Type.String(),
+		email: Type.String(),
+	}),
+})
