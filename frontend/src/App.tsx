2251b2a6a0e00a9b419146d18fdcd35e60db3cef
eelaine <eelaine@student.hive.fi>
2026-01-29 13:04:25 +0200

    removed unused variables defined in ProtectedRoutes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,24 +1,14 @@
 import React from "react";
 import { Outlet, useLocation } from "react-router-dom";
-import Header from "./components/Header";
-import Footer from "./components/Footer";
 
 const App: React.FC = () => {
-  const location = useLocation();
-
-  const noHeaderRoutes = ["/", "/register"];
-  const validRoutes = ["/", "/register", "/menu", "/friends", "/singlematch", "/profile"];
-  const showHeader =
-    !noHeaderRoutes.includes(location.pathname) &&
-    validRoutes.includes(location.pathname);
+  
 
   return (
     <div className="app-container">
-      {showHeader && <Header />}
 	  <main className="flex-grow">
 		<Outlet />
 	  </main>
-      {showHeader && <Footer />}
     </div>
   );
 };
