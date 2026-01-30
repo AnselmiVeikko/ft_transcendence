a91762288a35c815412bd40336ded2bda4d0094e
eelaine <eelaine@student.hive.fi>
2026-01-30 14:35:00 +0200

    added avatar functionality into useUser

--- frontend/src/hooks/useUser.ts
+++ frontend/src/hooks/useUser.ts
@@ -5,21 +5,26 @@ export const useUser = () => {
   const [userName, setUserName] = useState('');
   const [userId, setUserId] = useState('');
   const [email, setEmail] = useState('');
+  const [avatarURL, setAvatar] = useState('');
   const [loading, setLoading] = useState(true);
-
 
   const fetchUser = async () => {
     try {
       const { data } = await apiClient.get('/api/user/profile/self');
-      const { userName, userId, email } = data.data;
+      const { userName, userId, email, avatarName } = data.data;
       setUserName(userName);
       setUserId(userId);
       setEmail(email);
+
+	  const newURL = avatarName ? `/avatars/upload/${avatarName}?t=${Date.now()}` 
+        : '/avatars/default/default.webp';
+
+	  setAvatar(newURL);
     } catch (e) {
-      //console.error('Failed to fetch user:', e);
       setUserName('');
       setUserId('');
       setEmail('');
+	  setAvatar('');
     } finally {
       setLoading(false);
     }
@@ -29,5 +34,5 @@ export const useUser = () => {
     fetchUser();
   }, []);
 
-  return { userName, userId, loading, email, refetch: fetchUser };
+  return { userName, userId, loading, email, avatarURL, refetch: fetchUser };
 };
