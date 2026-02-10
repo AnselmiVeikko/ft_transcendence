e4242634976c2e79b5b3a75e8a537bc23cde2990
Anselmi Henttonen <AnselmiVeikko@gmail.com>
2026-02-10 15:20:57 +0200

    refactored match finish route to work better with game module logic

--- backend/src/routes/FinishMatch.ts
+++ backend/src/routes/FinishMatch.ts
@@ -4,8 +4,9 @@ import jwt from "jsonwebtoken";
 import { errorResponse } from "../utils/UserResponses";
 import { ErrorResponseSchema } from "../schemas/UserSchema";
 import { FinishMatchBodySchema, FinishMatchResponseSchema } from "../schemas/GameSchema";
+import { error } from "node:console";
 
-type FinishMatchRequest = FastifyRequest<{ Body: { matchId: string; gameToken: string; } }>;
+type FinishMatchRequest = FastifyRequest<{ Body: { matchId: string; winnerId: string; score: Record<string, number>; } }>;
 
 
 export default async function finishMatchRoute(app: FastifyInstance) {
@@ -18,20 +19,25 @@ export default async function finishMatchRoute(app: FastifyInstance) {
             },
         },
     }, async (request: FinishMatchRequest, reply: FastifyReply) => {
+        const authHeader = request.headers['authorization'];
+        if (!authHeader || !authHeader.startsWith('Bearer ')) {
+            return reply.status(500).send(errorResponse(400, "No authentication provided"));
+        }
+        const token = authHeader.slice(7);
         const secret = process.env.GAME_TOKEN_SECRET || "game-secret-change-this";
         if (!secret) {
             app.log.error("GAME_TOKEN_SECRET not set in environment ");
             return reply.status(500).send(errorResponse(500, "Server misconfiguration"));
         }
 
-        const { matchId, gameToken } = request.body;
+        const { matchId, winnerId } = request.body;
         if (!matchId)
             return reply.status(400).send(errorResponse(400, "No match id provided"));
-        if (!gameToken)
-            return reply.status(400).send(errorResponse(400, "No gameToken provided"));
-
+        if (!winnerId)
+            return reply.status(400).send(errorResponse(400, "No winner id provided"));
+        
         try {
-            jwt.verify(gameToken, secret);
+            jwt.verify(token, secret);
         } catch (err) {
             return reply.status(401).send(errorResponse(401, "Invalid or expired gameToken"));
         }
