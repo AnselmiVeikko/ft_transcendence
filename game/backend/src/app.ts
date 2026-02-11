0f56157dd9be8d3b7750e7399e2fc8a1ebece9ac
Anselmi Henttonen <AnselmiVeikko@gmail.com>
2026-02-11 14:35:03 +0200

    removed gameManager, as it was a legacy file

--- game/backend/src/app.ts
+++ game/backend/src/app.ts
@@ -2,7 +2,6 @@ import express, { type Request, type Response } from 'express';
 import { createServer } from 'http';
 import { WebSocketServer, WebSocket } from 'ws';
 import cors from 'cors';
-import { GameManager } from './core/gameManager.js';
 import { verifyJWT, extractAuthParams } from './utils/jwt.js';
 import { matchManager } from './core/matchManager.js';
 import { getOrCreateSession } from './core/matchGameSession.js';
