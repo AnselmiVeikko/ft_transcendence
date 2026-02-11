76f46f6550908181a547f7f5e915bf7f22f7a7fa
Anselmi Henttonen <AnselmiVeikko@gmail.com>
2026-02-11 14:34:38 +0200

    modified gane backend to send the mock JWT. It is not a real, signed JWT but a placeholder in case we establish authentication for game_be->main_be requests.

--- game/backend/src/utils/matchResult.ts
+++ game/backend/src/utils/matchResult.ts
@@ -41,16 +41,16 @@ export function buildScoreFromState(
 export async function reportResultToMainBE(
   matchId: string,
   winnerId: string,
-  gameToken: string | null,
   ){
   try {
-    const url = "http://backend:3000/api/game/finish";
+    const url = "http://backend:3000/api/game/matchResult";
     const body: MatchResult = { matchId, winnerId };
+    const token = getGameServiceToken()
     const res = await fetch(url, {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
-        ...(gameToken && { Authorization: `Bearer ${gameToken}` }),
+        ...(token && { Authorization: `Bearer ${token}` }),
       },
       body: JSON.stringify(body),
     });
