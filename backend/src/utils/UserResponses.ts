bc60cfc7c6dd03aad080fde55ee6de7601e41b25
shahnajsc <sschowdhury29@gmail.com>
2026-02-24 13:21:44 +0200

    Updated user avatar response body

--- backend/src/utils/UserResponses.ts
+++ backend/src/utils/UserResponses.ts
@@ -54,14 +54,9 @@ export function errorResponse(status: number, message: string) {
 }
 
 // AVATAR
-export function profileAvatarSet(user: { userId: string, userName: string, avatarName: string | null }){
+export function profileAvatarSet(){
 	return {
-	message: "Profile avatar change successful",
-		data: {
-			userId: user.userId,
-			userName: user.userName,
-			avatarName: user.avatarName,
-		},
+		message: "Profile avatar change successful",
 	};
 }
 
