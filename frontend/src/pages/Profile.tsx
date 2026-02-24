a0241e37fccfbde42091b2c4c0e577f4fe00f0b9
eelaine <eelaine@student.hive.fi>
2026-02-24 11:08:38 +0200

    fix Profile screen height on small screens by specifying min-h-screen

--- frontend/src/pages/Profile.tsx
+++ frontend/src/pages/Profile.tsx
@@ -71,12 +71,12 @@ const ProfileSettings = () => {
   if (loading) return null;
 
   return (
-    <div className="relative">
+    <div className="min-h-screen relative">
       <SettingsMenu />
 
       <div
-        className={`flex flex-col items-center justify-center p-4 w-full`}
-        /* style={{ height: 'calc(100vh - 5rem)' }} */
+        className={`flex flex-col grow items-center justify-center p-4 w-full`}
+        style={{ height: 'calc(100vh - 11rem)' }}
       >
         <div className="mb-4 p-6 text-center max-w-5xl w-full z-10">
           <h1 className="text-2xl font-extrabold tracking-tight sm:pb-2 bg-clip-text text-transparent bg-linear-to-r from-indigo-700 to-blue-200 sm:text-4xl lg:text-5xl">
