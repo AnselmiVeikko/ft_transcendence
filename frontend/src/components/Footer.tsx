e3b0f4b6ead132f6790c918ee6eeb11a07b681b5
eelaine <eelaine@student.hive.fi>
2026-02-24 14:35:35 +0200

    Removed year from footer, visual tweaks

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -36,14 +36,11 @@ const Footer = () => {
 
   return (
 	<>
-    <footer className="w-full py-3 relative mt-12 left-1/2 -translate-x-1/2 z-10 max-w-fit px-8 
+    <footer className="w-full py-3 relative z-10 max-w-fit px-4 
                    rounded-full border border-indigo-200/20 dark:border-indigo-900/30 
                    bg-white/80 dark:bg-gray-900/80 backdrop-blur-md 
                    shadow-lg shadow-indigo-500/10 transition-all transform hover:border-indigo-500/40 hover:shadow-indigo-500/20 hover:duration-300">
       <div className="max-w-7xl mx-auto px-4 flex md:flex-row justify-center items-center gap-8">
-        <p className="text-xs md:text-sm bg-clip-text text-transparent bg-linear-to-r from-indigo-500 dark:from-indigo-400 to-blue-400 hover:bg-linear-to-bl active:scale-95">
-          © {new Date().getFullYear()} PONG
-        </p>
 
         <div className="flex space-x-6">
           <button
