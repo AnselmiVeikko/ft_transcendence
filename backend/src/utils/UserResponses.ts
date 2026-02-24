3492aca4cdafcb80fdb0b43a096df1c3dcc8ed37
shahnajsc <sschowdhury29@gmail.com>
2026-02-24 14:38:39 +0200

    Updated schema for profile view

--- backend/src/utils/UserResponses.ts
+++ backend/src/utils/UserResponses.ts
@@ -22,11 +22,10 @@ export function accessRefreshed() {
 	};
 }
 
-export function profileSelf(userProfile: {userId: string, userName: string, email: string}, avatarUrl: string) {
+export function profileSelf(userProfile: {userName: string, email: string}, avatarUrl: string) {
 	return {
 		message: "Profile retrieve successful",
 		data: {
-			userId: userProfile.userId,
 			userName: userProfile.userName,
 			email: userProfile.email,
 			avatarUrl: avatarUrl,
