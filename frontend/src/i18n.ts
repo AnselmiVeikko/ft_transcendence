8021292f187a3a13d60c8ba89363346454a415ac
eelaine <eelaine@student.hive.fi>
2026-02-25 13:14:01 +0200

    cleanup i18n and default to en

--- frontend/src/i18n.ts
+++ frontend/src/i18n.ts
@@ -1,46 +1,33 @@
 import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
-import LanguageDetector from 'i18next-browser-languagedetector';
 import Backend from 'i18next-http-backend';
 
 i18n
-  // Enable i18next-http-backend to load translations
   .use(Backend)
-  // Enable i18next-browser-languagedetector to detect user language
-  .use(LanguageDetector)
-  // Pass the i18n instance to react-i18next
   .use(initReactI18next)
-  // Initialize i18next
   .init({
-    // Define namespaces (we only have 'translation' for now)
     ns: ['translation'],
     defaultNS: 'translation', 
-    
-    // Fallback language in case a translation is missing
+
     fallbackLng: 'en',
     
-    // Configuration for the http-backend
     backend: {
-      loadPath: '/locales/{{lng}}/{{ns}}.json', // Path to your translation files in the public folder
+      loadPath: '/locales/{{lng}}/{{ns}}.json',
     },
     
-    // Cache language selection
     detection: {
       order: ['queryString', 'cookie', 'localStorage', 'sessionStorage', 'navigator', 'htmlTag'],
       caches: ['localStorage'],
     },
     
-    // React settings
     react: {
-      useSuspense: true, // Allows using React.Suspense during loading
+      useSuspense: true,
     },
     
-    // Do not escape values as React already protects against XSS
     interpolation: {
       escapeValue: false, 
     },
     
-    //debug: process.env.NODE_ENV === 'development',
   });
 
 export default i18n;
